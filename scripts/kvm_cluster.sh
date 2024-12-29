8c07b401564e54d3a01100316b4c65939cccc126
Rickard Lundin <rickard@ignalina.dk>
2024-12-29 17:14:35 +0100

    should wok

--- scripts/kvm_cluster.sh
+++ scripts/kvm_cluster.sh
@@ -7,7 +7,6 @@ os_version=$1
 machines=$2
 host_name=$3
 
-nuke_all_vm
 
 for (( i=1; i<${machines}; ++i)); do
     install_node_image $os_version machine${i}.${host_name} &
--- scripts/kvm_initiate.sh
+++ scripts/kvm_initiate.sh
@@ -7,6 +7,7 @@ os_version=$1
 machines=$2
 host_name=$3
 
+nuke_all_vm
 
 for (( i=1; i<${machines}; ++i)); do
     create_node_image $os_version machine${i}.${host_name}
