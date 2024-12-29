7b59c92c4294277af733bf1b06225f2b194bcc6e
Rickard Lundin <rickard@ignalina.dk>
2024-12-29 17:59:16 +0100

    fixed

---
+++ scripts/redfish_cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source core
+
+
+os_version=$1
+machines=$2
+host_name=$3
+bmcs=$4
+
+
+for (( i=1; i<${machines}; ++i)); do
+    install_redfish_node_image $os_version machine${i}.${host_name} ${bmcs} &
+done
+
