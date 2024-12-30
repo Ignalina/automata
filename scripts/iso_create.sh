31f4d815c604e10d4db0cd17eabb26475ffc52d2
Rickard Lundin <rickard@ignalina.dk>
2024-12-30 01:30:27 +0100

    fixed

--- scripts/iso_create.sh
+++ scripts/iso_create.sh
@@ -5,10 +5,12 @@ source core
 
 os_version=$1
 machines=$2
-host_name=$3
+
+node_name=$3
+domain_name=$4
 
 
 
 for (( i=1; i<${machines}; ++i)); do
-    create_kvm_node_iso $os_version machine${i}.${host_name}
+    create_kvm_node_iso $os_version ${node_name}${i}.${domain_name}
 done
