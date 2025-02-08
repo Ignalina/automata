e6f6f1456bba8da8d8677c075ec240e91b9b2a94
Rickard Lundin <rickard@ignalina.dk>
2025-02-08 16:19:53 +0100

    dataplatform

---
+++ scripts/complete_dataplatform.sh
@@ -0,0 +1,18 @@
+#rm -rf ~/vms/virsh/images
+#rm -rf ~/vms/virsh/xml
+#rm -rf ~/vms/virsh/init
+
+os_version=ubi_9_5
+
+
+./kvm_nuke_all.sh ${os_version}
+./airgap_media.sh ${os_version}
+
+
+./iso_create.sh ${os_version} 3 spark x14.se
+./kvm_initiate.sh ${os_version} 3 spark x14.se
+./kvm_cluster.sh ${os_version} 3 spark x14.se
+
+./iso_create.sh ${os_version} 6 doris x14.se
+./kvm_initiate.sh ${os_version} 6 doris x14.se
+./kvm_cluster.sh ${os_version} 6 doris x14.se
