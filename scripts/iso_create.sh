034914f32daee8b7ed11b9086bc016cf1da86b7a
Rickard Ernst Björn Lundin <rickard@x14.se>
2025-08-12 03:13:04 +0200

    correct fqdn

--- scripts/iso_create.sh
+++ scripts/iso_create.sh
@@ -8,10 +8,11 @@ os_version=$1
 machines=$2
 
 node_name=$3
-domain_name=$4
+domain_name="$4"
 
+ECHO "DOMAIN NAME=${domain_name}"
 
 
 for (( i=1; i<=${machines}; ++i)); do
-    create_kvm_node_iso $os_version ${node_name}${i}.${domain_name}
+    create_kvm_node_iso $os_version "${node_name}${i}.${domain_name}"
 done
