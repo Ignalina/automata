9d1fa22350bfc518853cf7dce04970dba4a2937b
Rickard Ernst Björn Lundin <rickard@x14.se>
2025-08-20 20:54:04 +0200

    okk

--- scripts/spark_dataplattform.sh
+++ scripts/spark_dataplattform.sh
@@ -6,7 +6,7 @@
 os_version=ubuntu_22_04
 export CLOUDINIT="cloudinit25"
 export PUBLIC_KEY=$(< ~/.ssh/id_ed25519.pub)
-export VM_IMAGE_DIR="/mnt/md0/vms/virsh"
+export VM_IMAGE_DIR="/mnt/xfs/vms/virsh"
 
 
 ./kvm_nuke_all.sh ${os_version}
